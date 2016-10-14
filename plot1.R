6b193e63f1490852b3e951835637eeaad6595b55
rocco.sorma <rocco.sorma@safeguardproperties.com>
2016-10-14 10:58:02 -0400

    This is the first commit for plot1 R and png files

---
+++ plot1.R
@@ -0,0 +1,20 @@
+SCC <- readRDS("Source_Classification_Code.rds")
+NEI <- readRDS("summarySCC_PM25.rds")
+
+## Aggregate emmision by year.
+
+total <- data.frame(aggregate(as.numeric(NEI$Emissions), by = list(NEI$year), FUN = sum))
+total
+total$x <- round(total[, 2]/1000)
+
+png(filename='plot1.png')
+
+ylim <- c(0, 1.1*max(total$x))
+
+xx <- barplot(total$x, names = total$Group.1, 
+        main = "PM2.5 Emissions by Year", xlab = "Year", ylab = "PM2.5 In Kilotons",
+        width = 0.85, ylim = ylim)
+text(x = xx, y = total$x, label = total$x, pos = 3, cex = 1)
+
+
+dev.off()
