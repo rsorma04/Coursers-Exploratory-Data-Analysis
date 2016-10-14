e99828aaf9ba2406eb9bfcfa0e7674a7374a1213
rocco.sorma <rocco.sorma@safeguardproperties.com>
2016-10-14 14:58:47 -0400

    This is the first commit for plot3.R

---
+++ plot3.R
@@ -0,0 +1,23 @@
+library(ggplot2)
+
+SCC <- readRDS("Source_Classification_Code.rds")
+NEI <- readRDS("summarySCC_PM25.rds")
+
+## Aggregate emmision by year.
+balt_total <- subset(NEI, NEI$fips == 24510)
+total <- data.frame(aggregate(as.numeric(balt_total$Emissions), by = list(balt_total$year,
+                                                                          balt_total$type), 
+                              FUN = sum))
+total$x <- round(total[, 3]/1000, 2)
+
+png(filename='plot3.png')
+
+qplot(data=total, x=Group.1, y=x, color=Group.2, facets = ~Group.2) 
+
+ggplot(data = total, aes(x = factor(Group.1), y = x, fill=Group.2)) + 
+  geom_bar(stat = 'identity') +
+  scale_fill_brewer(palette="Set1") +
+  facet_grid(facets = ~Group.2)
+
+
+dev.off()
